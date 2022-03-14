520dec56e75d760fc2fe2e825ba653a9b5ca77e5
Shadow <borntodevelop77@gmail.com>
2022-03-14 18:12:12 +0530

    'Commit'

---
+++ Discord_bot/main.py
@@ -0,0 +1,84 @@
+from turtle import title
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import requests
+
+client = commands.Bot(command_prefix = '$')
+#ok
+
+
+#login event
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+#join event
+@client.event
+async def on_member_join(member):
+    print(f'{member} joined the server')
+
+#remove event
+@client.event
+async def on_member_remove(member):
+    print(f'{member} left the server')
+
+@client.command()
+async def stats(message):
+    # if message.author == client.user:
+    #     return
+
+    # id = client.get_guild(950674739133808690)
+    name = str(message.guild.name)
+    owner_name = str(message.guild.owner)
+    server_id = str(message.guild.id)
+    no_of_channels = str(message.guild.channels)
+    member_count = str(message.guild.member_count)
+
+    icon = str(message.guild.icon_url)
+
+    embed = discord.Embed(
+        title = "Server Stats",
+        color = discord.Color.dark_red()
+    )
+    embed.set_thumbnail(url=icon)
+
+    embed.add_field(name="Server Name", value=name ,inline=True)
+    embed.add_field(name="Owner name", value=owner_name ,inline=True)
+    embed.add_field(name="Server id", value=server_id ,inline=True)
+    # embed.add_field(name="no of channel", value=no_of_channels ,inline=True)
+    embed.add_field(name="Member Count", value=member_count ,inline=True)
+
+    await message.send(embed=embed)
+    # if message.content.startswith('$hello'):
+    #     slug = message.content.partition('')[2]
+    #     print(slug)
+    #     await message.channel.send('Hello!')
+
+    # if message.content.startswith('$server_stats'):
+    # await message.send(f' Server Stats:-\n no of member: {id.member_count}')
+  
+@client.command()
+async def nft(message,*,slug):
+    response = requests.get(f'https://api.opensea.io/collection/{slug}/stats').json()['stats']
+    
+    icon = requests.get(f'https://api.opensea.io/collection/{slug}').json()['collection']['primary_asset_contracts'][0]['image_url']
+    
+    embed = discord.Embed(
+        title = f"NFT = {slug}",
+        description = "NFT Stats",
+        color = discord.Color.dark_red()
+    )
+    embed.set_thumbnail(url=icon)
+
+    for key ,value in response.items():
+        embed.add_field(name=key, value=value ,inline=True)
+    
+    await message.send(embed=embed)
+    
+
+
+
+load_dotenv()
+client.run(os.getenv('TOKEN'))
---
+++ Telegram_bot/main.py
@@ -0,0 +1,109 @@
+from telegram import *
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from uuid import uuid4
+import sys
+from sqlalchemy.orm import Session, sessionmaker
+import requests
+from sqlalchemy import create_engine
+from sqlalchemy.sql import exists
+import json
+
+
+sys.path.append(".")
+from models import *
+
+from Telegram_bot.utils import get_collection_id
+
+
+#engine = create_engine('postgresql://spqqojmysvclhl:35e13032f8326f8b7908e52a75e65215a62437d5c0c618aaee8a14392405e188@ec2-52-204-14-80.compute-1.amazonaws.com:5432/d7jch8clhgaktb', echo=False)
+
+engine = create_engine('postgresql+psycopg2://postgres:aoGY0J9U9o@hypemail-db-staging.c44vnyfhjrjn.us-east-1.rds.amazonaws.com:5432/nft-bot', echo=False)
+
+Session = sessionmaker(bind=engine)
+Session.configure(bind=engine)
+session = Session()
+
+
+
+TOKEN = '5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k'
+
+
+#bot start command    
+def start(update, context):
+
+    update.message.reply_text("""Hello,\n Welcome to NFT Collection Bot \nFor more - /help""")
+
+    user_chat_id = update.effective_user.id
+    user_chat_type = update.effective_chat.type
+
+    user = User(username=update.effective_user.username,chat_id=user_chat_id,chat_type=user_chat_type)
+
+    if not session.query(session.query(User).filter_by(chat_id=user_chat_id).exists()).scalar():
+        session.add(user)
+        print('User added')
+
+    
+    session.commit()
+   
+
+
+#bot help command
+def help (update, context):
+    update.message.reply_text("""
+    Commands:
+    /help - to view commands
+    /add_collection <YOUR_NFT_COLLECTION_NAME> - to add NFT collection 
+    """)
+
+
+        
+#bot command to get collection data 
+def add_collection(update, context):
+    url = update.message.text.partition(' ')[2]
+    slug = url.split('/')[-1]
+    if url != '':
+        update.message.reply_text(f"NFT Collection Name - {slug}")
+    
+        user = session.query(User).filter_by(chat_id=update.effective_user.id).first()
+        collection_id = get_collection_id(slug)
+        
+        resp = requests.get(f'https://api.opensea.io/collection/{slug}/stats').json()['stats']
+
+        # if not session.query(session.query(Collection, User).filter(User_Collection.collection_id==collection_id, User_Collection.user_id==user.id).exists()).scalar():
+        if session.query(User_Collection).filter(User_Collection.collection_id==collection_id, User_Collection.user_id==user.id).count() == 0:
+            collection = session.query(Collection).filter_by(id=collection_id).first()
+            user.collections.append(collection)
+            print('Collection added')
+            update.message.reply_text(f"NFT Collection Name - {slug}\nIs added to our Database")
+            session.commit()
+        else:
+            update.message.reply_text("Error - Invaild Text or collection is already there, please use /start and then /help to know commands")
+    else:
+        update.message.reply_text("Error - Invaild Text, Use /help to know more")
+
+
+#message handling command
+def handle_message(update,context):
+    message = update.message.text
+    print(update.effective_user.id)
+    print(update.effective_user.username)
+    if message in ("Hi","Hii","hii","hello","Hello"):
+        update.message.reply_text(f"{message}, there")
+    else:
+        update.message.reply_text("Error - Invalid Text , use /help ")
+
+
+if __name__ == '__main__':
+    updater = Updater('5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k', use_context=True)
+    dp = updater.dispatcher
+    print("started")
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("add_collection", add_collection))
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
+    
+
+    updater.start_polling()
+    updater.idle()
+
+
